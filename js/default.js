f886349a8445ef30b76d66b8169167b681d97ca4
jsjhass <jsjhass@gmail.com>
2017-12-31 19:01:44 +0530

    Second commit

---
+++ js/default.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $.ajaxSetup({ cache: false });
+    $('#pagecontent').load("home.html");
+    $('#home').click(function () {
+        $('#pagecontent').hide().load("home.html").fadeIn("slow");
+    });
+    $('#about').click(function () {
+        $('#pagecontent').hide().load("about.html").fadeIn("slow");
+    });
+    $('#article').click(function () {
+        $('#pagecontent').hide().load("article.html").fadeIn("slow");
+    });
+    $('#project').click(function () {
+        $('#pagecontent').hide().load("project.html").fadeIn("slow");
+    });
+    $('#contact').click(function () {
+        $('#pagecontent').hide().load("contact.html").fadeIn("slow");
+    });
+});
